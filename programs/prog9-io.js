5539408c6398475bc000f36f39b6d5fb8c9a0d97
adarshpandya-simform <adarsh.p@simformsolutions.com>
2021-12-23 15:40:23 +0530

    added : char practise toggle program

---
+++ programs/prog9-io.js
@@ -0,0 +1,42 @@
+/**
+ * You have been given a String S consisting of uppercase and lowercase English alphabets. You need to change the case of each alphabet in this String. That is, all the uppercase letters should be converted to lowercase and all the lowercase letters should be converted to uppercase. You need to then print the resultant String to output.
+
+Input Format
+The first and only line of input contains the String S
+
+Output Format
+Print the resultant String on a single line.
+
+Constraints
+
+where S denotes the length of string S.
+Sample Input
+
+abcdE
+
+Sample Output
+
+ABCDe
+ */
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+var stdin_input = "";
+
+process.stdin.on("data", function (input) {
+  stdin_input += input; // Reading input from STDIN
+});
+
+process.stdin.on("end", function () {
+  main(stdin_input);
+});
+
+function main(input) {
+  let str = input.split("");
+  let toggle = str
+    .map((char) =>
+      char == char.toLowerCase() ? char.toUpperCase() : char.toLowerCase()
+    )
+    .join("");
+  console.log(toggle);
+}
